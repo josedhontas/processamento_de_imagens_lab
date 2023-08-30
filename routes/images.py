c32bd667b47267c6949cc3259a36fe58c137d63b
josedhonatas <dhonatas@academico.ufs.br>
2023-08-30 14:11:49 -0300

    nova_rota

--- routes/images.py
+++ routes/images.py
@@ -38,24 +38,24 @@ def get_gray_image(image_id):
 def get_histeq_image(image_id):
     image_path = f'img/{image_id}.jpg'
     img = imread(image_path)
-    img = histeq(img)
-    image = imshow(img)
-    return send_file(image, mimetype='image/png')
+    img_histeq = histeq(img)
+    img_out = imshow(img_histeq)
+    return send_file(img_out, mimetype='image/png')
 
 @images_bp.route('/blur/<int:image_id>')
 def get_blur_image(image_id):
     image_path = f'img/{image_id}.jpg'
-    img = imread(image_path)
-    img = blur(img)
-    image = imshow(img)
-    return send_file(image, mimetype='image/png')
+    image = imread(image_path)
+    image_blur = blur(image)
+    image_out = imshow(image_blur)
+    return send_file(image_out, mimetype='image/png')
 
 @images_bp.route('/erode/<int:image_id>')
 def get_erode_image(image_id):
     image_path = f'img/{image_id}.jpg'
-    img = imread(image_path)
+    image = imread(image_path)
     element = seSquare3()
-    img = erode(img, element)
-    image = imshow(img)
-    return send_file(image, mimetype='image/png')
+    image_erode = erode(image, element)
+    image_out = imshow(image_erode)
+    return send_file(image_out, mimetype='image/png')
 
