466d710c5174b639bc6db4a8ff96e4bfe6921bac
josedhonatas <dhonatas@academico.ufs.br>
2023-08-31 10:21:19 -0300

    requirements

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask
+from flask_cors import CORS
 from routes.images import images_bp
 
 app = Flask(__name__)
+CORS(app)  
 
 app.register_blueprint(images_bp, url_prefix='/images')
 
